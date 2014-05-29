dd385eed31fa7c8ddbd3ac70f88f3bd598d3d143
Ben0xA <web@ben0xa.com>
2014-05-29 09:15:34 -0500

    Making this look more like the powershell cmdlet

--- Get-Order/GetOrder.cs
+++ Get-Order/GetOrder.cs
@@ -1,44 +1,32 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.Management.Automation;
-using System.Text;
 
 namespace GetOrder
 {
     [Cmdlet(VerbsCommon.Get, "Order")]
     public class GetOrder : PSCmdlet
     {
-        int _cups;
-        string _product = "Lemonade";
+        [Parameter(
+            Mandatory = true,
+            Position = 1,
+            HelpMessage = "How many cups would you like to purchase?"
+        )]
+        public int Cups;
 
         [Parameter(
-            Mandatory=true, 
-            Position=1, 
-            HelpMessage="How many cups would you like to purchase?"
-        )]        
-        public int Cups
-        {
-            set { _cups = value; }
-            get { return _cups; }
-        }
-
-        [Parameter(
-            Mandatory=false, 
-            Position=2, 
-            HelpMessage="What would you like to purchase?"
+            Mandatory = false, 
+            Position = 2, 
+            HelpMessage = "What would you like to purchase?"
         )]
         [ValidateSet("Lemonade","Water","Tea","Coffee")]
-        public string Product
-        {
-            set { _product = value; }
-            get { return _product; }
-        }
+        public string Product = "Lemonade";
 
         protected override void ProcessRecord()
         {
             Collection<String> order = new Collection<string>();
-            for (int cup = 1; cup <= _cups; cup++) {
-                order.Add(cup.ToString() + ": A cup of " + _product);
+            for (int cup = 1; cup <= Cups; cup++) {
+                order.Add(cup.ToString() + ": A cup of " + Product);
             }
             WriteObject(order, true);
         }
