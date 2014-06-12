3f5abfb5543b6fe228e8fee52a281efbcc722145
Ben0xA <web@ben0xa.com>
2014-06-12 10:02:40 -0500

    Final Code
    Adding the changes for the final code for reference if need be.

--- Get-Order/GetOrder.cs
+++ Get-Order/GetOrder.cs
@@ -5,8 +5,10 @@ using System.Management.Automation;
 namespace GetOrder
 {
     [Cmdlet(VerbsCommon.Get, "Order")]
-    public class GetOrder : PSCmdlet
+    public class GetOrder : PSCmdlet, IDynamicParameters
     {
+        private AgeDynamicParameter agedynparm = null;
+        
         [Parameter(
             Mandatory = true,
             Position = 1,
@@ -19,8 +21,31 @@ namespace GetOrder
             Position = 2, 
             HelpMessage = "What would you like to purchase?"
         )]
-        [ValidateSet("Lemonade","Water","Tea","Coffee")]
+        [ValidateSet("Lemonade","Water","Tea","Coffee","Hard Lemonade")]
         public string Product = "Lemonade";
+
+        public object GetDynamicParameters()
+        {
+            if (Product == "Hard Lemonade")
+            {
+                agedynparm = new AgeDynamicParameter();
+                return agedynparm;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        protected override void BeginProcessing()
+        {
+            if (agedynparm != null && agedynparm.Age < 21)
+            {
+                ParameterBindingException pbe = new ParameterBindingException("You are not old enough for Hard Lemonade. How about a nice glass of regular Lemonade instead?");
+                ErrorRecord erec = new ErrorRecord(pbe, null, ErrorCategory.PermissionDenied, agedynparm.Age);
+                ThrowTerminatingError(erec);                
+            }
+        }
 
         protected override void ProcessRecord()
         {
@@ -30,5 +55,21 @@ namespace GetOrder
             }
             WriteObject(order, true);
         }
+    }
+
+    public class AgeDynamicParameter
+    {
+        private int _age;
+        
+        [Parameter(
+            Mandatory = true,
+            Position = 3,
+            HelpMessage = "Please enter your age:"
+        )]
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }        
     }
 }
